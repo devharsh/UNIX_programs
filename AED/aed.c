6016dc41c8f42749298f38afd3f27a3d0186f2d3
Devharsh Trivedi <devharsh@live.in>
2019-11-22 03:17:45 -0500

    aed-sym-check-point-3

--- AED/aed.c
+++ AED/aed.c
@@ -4,39 +4,38 @@ int
 main(void) {
 	char buf[SIZE];
 
-	unsigned char key[32];
+	/*unsigned char key[32];
 	unsigned char iv[16];
-	unsigned char *plaintext = NULL;
-	
-	generate_key(key, iv);
 
-	while(fgets(buf, SIZE, stdin) != NULL) {
-		if(!plaintext) {
-			plaintext = (unsigned char*)(&buf);
-		} else {
-			strncat((char*)plaintext, buf, strlen(buf));		
-		}
-	}
+	generate_key(key, iv);*/
 
-	if(plaintext != NULL) {
+	/* A 256 bit key */
+    	unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+
+    	/* A 128 bit IV */
+    	unsigned char *iv = (unsigned char *)"0123456789012345";
+
+	while (fgets(buf, SIZE, stdin) != NULL) {
+		/*unsigned char plaintext[SIZE];*/
     		unsigned char ciphertext[SIZE];
     		unsigned char decryptedtext[SIZE];
 
-    		int decryptedtext_len, ciphertext_len;
+    		int ciphertext_len, decryptedtext_len;
 
-    		ciphertext_len = aed_encrypt (plaintext, strlen ((char *)plaintext), key, iv,
+		if(ciphertext_len > decryptedtext_len) {}	
+		
+		/*memcpy(plaintext, buf, strlen(buf));
+		ciphertext_len = aed_encrypt(plaintext, strlen ((char *)plaintext), key, iv,
                               ciphertext);
+		printf("%s", ciphertext);*/
 
-    		BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
-
-    		decryptedtext_len = aed_decrypt(ciphertext, ciphertext_len, key, iv,
+		memcpy(ciphertext, buf, strlen(buf));
+		decryptedtext_len = aed_decrypt(ciphertext, strlen ((char *)ciphertext), key, iv,
                                 decryptedtext);
-
-    		decryptedtext[decryptedtext_len] = '\0';
-
-    		printf("%s\n", decryptedtext);
+		decryptedtext[decryptedtext_len] = '\0';
+		printf("%s", decryptedtext);
 	}
-	
+
 	return 0;
 }
 
@@ -84,54 +83,90 @@ int
 aed_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
         unsigned char *iv, unsigned char *ciphertext)
 {
-    	EVP_CIPHER_CTX *ctx;
+    EVP_CIPHER_CTX *ctx;
 
-    	int len;
-    	int ciphertext_len;
+    int len;
 
-    	if(!(ctx = EVP_CIPHER_CTX_new()))
-        	handleErrors();
+    int ciphertext_len;
 
-    	if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-        	handleErrors();
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        handleErrors();
 
-    	if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-        	handleErrors();
-    	ciphertext_len = len;
+    /*
+     * Initialise the encryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits
+     */
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        handleErrors();
 
-    	if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
-        	handleErrors();
-    	ciphertext_len += len;
+    /*
+     * Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+        handleErrors();
+    ciphertext_len = len;
 
-    	EVP_CIPHER_CTX_free(ctx);
+    /*
+     * Finalise the encryption. Further ciphertext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+        handleErrors();
+    ciphertext_len += len;
 
-    	return ciphertext_len;
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return ciphertext_len;
 }
 
 int 
 aed_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
         unsigned char *iv, unsigned char *plaintext)
 {
-    	EVP_CIPHER_CTX *ctx;
+    EVP_CIPHER_CTX *ctx;
 
-    	int len;
-    	int plaintext_len;
+    int len;
 
-    	if(!(ctx = EVP_CIPHER_CTX_new()))
-        	handleErrors();
+    int plaintext_len;
 
-    	if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-        	handleErrors();
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        handleErrors();
 
-    	if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-        	handleErrors();
-    	plaintext_len = len;
+    /*
+     * Initialise the decryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits
+     */
+    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        handleErrors();
 
-    	if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
-        	handleErrors();
-    	plaintext_len += len;
+    /*
+     * Provide the message to be decrypted, and obtain the plaintext output.
+     * EVP_DecryptUpdate can be called multiple times if necessary.
+     */
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+        handleErrors();
+    plaintext_len = len;
 
-    	EVP_CIPHER_CTX_free(ctx);
+    /*
+     * Finalise the decryption. Further plaintext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+        handleErrors();
+    plaintext_len += len;
 
-    	return plaintext_len;
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return plaintext_len;
 }
